5fe1631724c4b8ded9f265e132b39fc4341b7552
moskalevNick1 <n.moskalev@freshlimesoft.com>
2022-06-23 18:37:21 +0300

    init

---
+++ src/app.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ProductsModule } from './products/products.module';
+
+@Module({
+  imports: [
+    ProductsModule,
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.DB_URL),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
